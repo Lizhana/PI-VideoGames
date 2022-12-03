2bf113f014ff50e2970f7b9cff6c7e0bb1a2faf7
Lizhana <lizhanita@gmail>
2022-12-03 20:40:17 -0300

    12-3-2022

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import './Css/landing.css'
+import land from'./imagenes/landing.gif'
 
 export default function LandingPage() {
    return ( 
    <div className="landing">
-       <h1>Comienza la experiencia Videogames</h1> 
+    <img src={land} alt="" />
+    <br /><br /><br />
+        <div className="title">
+        <h1>Welcome Videogames</h1>
+        </div>
+    <div>
+       
+       
         <Link to='/home'>
        <button>PLAY</button>
         </Link>
-    </div>)
+        <br /><br /><br /><br />
+   
+        </div>
+        </div>
+    )
 }
