58cebcf9dc8d02ef7a0252d2c369ca011a08f88c
Lizhana <lizhanita@gmail>
2022-12-07 16:47:17 -0300

    variablesBack

--- api/src/db.js
+++ api/src/db.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const { throws } = require('assert');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, API_KEY
+  DB_USER, DB_PASSWORD, DB_HOST, API_KEY, DB_NAME
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
